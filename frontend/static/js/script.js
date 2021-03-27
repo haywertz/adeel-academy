feb11ac3179d6becf8b70bcee15a2f34e347656c
haywertz <71106358+haywertz@users.noreply.github.com>
2021-03-27 17:56:47 -0500

    Update script.js

--- frontend/static/js/script.js
+++ frontend/static/js/script.js
@@ -1,5 +1,28 @@
+/*
+HTTP Status Codes:
+
+1xx – Informational
+	 The Client Request has either been Received or are Processing.
+2xx – Success 
+	 The Client Request was Successfully Received, Understood and Accepted.
+3xx – Redirect
+	 The Client Request was redirected and/or further action is required.
+4xx – Client Request Error
+	 The Client Request does not have what is necessary.
+5xx – Server Response Error 
+	 The Server has Failed to Fulfill a Valid Client Request.
+
+
+*/
 
 let url_base = "http://127.0.0.1:5000/" 
+
+// Sending HTTP Requests with the fetch API -> The URL in the Fetch Method is from regres.in and Allows to Test Front-End with API
+const getData = () => {
+    fetch("https://reqres.in/api/users");
+    
+};
+
 
 // Hamburger Menu Script
 const btnHamburger = document.querySelector('#btnHamburger');
